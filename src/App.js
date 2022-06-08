001a881a8e615a4f27c667114470b649dac21bbf
TuanPhan <ptuan343169@gmail.com>
2022-06-08 14:10:16 +0700

    UpdateTodoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import TodoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Tuan Phan
@@ -17,7 +19,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <TodoFeature />
     </div>
   );
 }
